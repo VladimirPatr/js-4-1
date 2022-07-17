308acb8327a18acee0c515f09a83715f6feba401
Vladimir Patrikeyev <patrik666@gmail.com>
2022-07-17 12:18:31 +0300

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+const productName = 'Acer Nitro 5';
+const quantitOfGoods = 420;
+const productСategory = 'laptop';
+const productPrice = 107400;
+
+console.log(productName);
+console.log(quantitOfGoods*productPrice)
